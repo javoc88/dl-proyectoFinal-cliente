1335781798b97164e3221c2e70177855ed45f2ad
Rcamino1 <127453851+Rcamino1@users.noreply.github.com>
2024-02-12 19:12:59 -0300

    App and Main restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import MyNavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
-const App = () => {
+function App() {
   return (
     <>
-      <MyNavBar />
-      <h1>NexGen License Store</h1>
-      <Footer />
+      <div className="navbar">
+        <MyNavBar />
+      </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/productos" element={<Home />} />
+          <Route path="/productos/:id" element={<Home />} />
+          <Route path="/cart" element={<Home />} />
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
   );
-};
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
