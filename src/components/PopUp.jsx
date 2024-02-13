b7de5017154acae09a4216778e6040500d97ec74
Javoc <javier.ochoa.77@gmail.com>
2024-02-12 23:00:32 -0300

    Feat added PopUp.jsx to show modal message
    This feature works when user add products to a cart

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const PopUp = ({
+  // escribir lógica de los botones y demás componentes aquí.
+  show,
+  handleClose,
+  onContinueShopping,
+  onCheckout,
+}) => {
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static"
+    keyboard={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Producto agregado</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Tu producto se ha agregado al carrito con éxito.</Modal.Body>
+      <Modal.Footer>
+        <Button variant="success" onClick={handleClose}>
+          Seguir comprando
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default PopUp;
