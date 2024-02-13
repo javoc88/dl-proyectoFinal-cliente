ce1a38f0da7af3cef28cd224f78bf9bf4deb7ab1
Javoc <javier.ochoa.77@gmail.com>
2024-02-13 19:39:49 -0300

    Fix css file import issues at Footer and Home, plus changed Home style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Footer.module.css";
+import styles from "../styles/Footer.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.home}>
+      <div className={styles.hero}>
+        <h1 className={styles.title}>Bienvenido a NexGen License Store</h1>
+        <p className={styles.description}>
+          Bienvenido a nuestro marketplace, donde ofrecemos precios
+          competitivos, una amplia gama de software y servicios adicionales para
+          satisfacer todas tus necesidades.
+        </p>
+        <button className={styles.button}>Start Now</button>
+      </div>
+      <div className={styles.background}></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
