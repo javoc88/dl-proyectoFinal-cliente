a1399e0320e11b804b46437f1660abcea3e4a2f0
Javoc <javier.ochoa.77@gmail.com>
2024-02-23 17:13:53 -0300

    Change style form and buttons

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -13,30 +14,40 @@ const LoginPage = () => {
 
   return (
     <div className="login-container">
-      <form onSubmit={handleSubmit} className="login-form">
+      <Form onSubmit={handleSubmit} className="login-form">
         <h2>Bienvenido/a</h2>
         <p>Inicia sesión con tu email y contraseña</p>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Contraseña"
-          required
-        />
-        <div className="login-remember">
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Recordar sesión</label>
-        </div>
-        <button type="submit">Login</button>
-        <a href="/registro">¿No tienes una cuenta? Crea una cuenta aquí</a>
-      </form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contraseña"
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Recordar sesión" />
+        </Form.Group>
+        <Button variant="secondary" type="submit">
+          Login
+        </Button>
+        <Form.Text className="text-muted">
+          ¿No tienes una cuenta?{" "}
+          <a href="/registro">
+            Crea una cuenta <strong>aquí</strong>
+          </a>
+        </Form.Text>
+      </Form>
     </div>
   );
 };
