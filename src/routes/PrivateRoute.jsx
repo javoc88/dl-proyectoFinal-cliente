592da9d02d917e69408c6883f6114336a34d6b44
Javoc <javier.ochoa.77@gmail.com>
2024-02-24 17:24:04 -0300

    Add private router

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Navigate, Route } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+const PrivateRoute = ({ element, ...rest }) => {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  return isLoggedIn ? <Route {...rest} element={element} /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
