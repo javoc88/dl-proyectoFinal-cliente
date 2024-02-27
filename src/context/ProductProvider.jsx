ff9cd919c4491d1131d13bd82595b6e0141b2571
Javoc <javier.ochoa.77@gmail.com>
2024-02-27 19:46:31 -0300

    Fix code missing issue

--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -29,7 +29,62 @@ const ProductProvider = ({ children }) => {
     return formatter.format(value);
   };
 
-  // Resto del código sin cambios
+  const addToCart = (product) => {
+    if (product) {
+      const productID = product.id ? product.id.toString() : "";
+      setCart((prevCart) => ({
+        ...prevCart,
+        [productID]: (prevCart[productID] || 0) + 1,
+      }));
+    } else {
+      console.warn(
+        "Intento de agregar un producto nula o indefinido al carrito."
+      );
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => {
+      const newCart = { ...prevCart };
+      delete newCart[id];
+      return newCart;
+    });
+  };
+
+  const calculateTotal = () => {
+    const total = Object.keys(cart).reduce((acc, productID) => {
+      const product = products.find((p) => p.id === productID);
+      if (product) {
+        return acc + parseFloat(product.precio) * cart[productID];
+      }
+      return acc;
+    }, 0);
+
+    return total.toFixed(2);
+  };
+
+  const GetCartTotal = () => {
+    const cartTotal = formatCurrency(calculateTotal());
+    return <span>Total: {cartTotal}</span>;
+  };
+
+  const GetTotalProductCount = () => {
+    const totalCount = Object.values(cart).reduce(
+      (acc, count) => acc + count,
+      0
+    );
+    return <span>Total Products: {totalCount}</span>;
+  };
+
+  const handleAddToCart = (product) => {
+    const { id, ...rest } = product;
+    addToCart({ ...rest, id: id ? id.toString() : "" });
+    setShowPopUp(true);
+  };
+
+  const handleClosePopUp = () => {
+    setShowPopUp(false);
+  };
 
   const contextValue = {
     products,
