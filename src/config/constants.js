864b6e2c8ac9979267e546f00142b65f4b580737
Javoc <javier.ochoa.77@gmail.com>
2024-02-29 17:54:23 -0300

    Fix API endpoint in constants.js with missing /api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = {
-  login: `${API_BASE_URL}/api/login`,
+  login: `${API_BASE_URL}/api/users/login`,
   users: `${API_BASE_URL}/api/users`,
 };
