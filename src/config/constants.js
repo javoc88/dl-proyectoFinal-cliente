21f3f94d6f0299aa6bdafdb00bfb87d6c32c46ea
Javoc <javier.ochoa.77@gmail.com>
2024-02-29 16:42:59 -0300

    Fix refactor endponit from constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = {
-  login: `${API_BASE_URL}/login`,
-  users: `${API_BASE_URL}/usuarios`
-}
+  login: `${API_BASE_URL}/users/login`,
+  users: `${API_BASE_URL}/users`,
+};
