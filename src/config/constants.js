f7037c28e02527b152eb446e045b0ec8fd15717a
Javoc <javier.ochoa.77@gmail.com>
2024-02-29 18:39:02 -0300

    Change API users endpoint in constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,5 +2,5 @@ export const API_BASE_URL = import.meta.env.VITE_APP_URL;
 
 export const ENDPOINT = {
   login: `${API_BASE_URL}/api/users/login`,
-  users: `${API_BASE_URL}/api/users`,
+  users: `${API_BASE_URL}/api/users/registro`,
 };
